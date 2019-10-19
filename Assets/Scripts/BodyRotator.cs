2270d15003627a3e49e20cf5fa992b5b9c907d30
Jimmy0001 <ahmedgamal000001@gmail.com>
2019-10-19 23:24:30 +0200

    Controls

---
+++ Assets/Scripts/BodyRotator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+  
+public class BodyRotator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject go;
+    public Joystick JS;
+    public float Speed=30;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (JS.Horizontal != 0)
+        {
+            go.transform.Rotate(Vector3.up, JS.Horizontal * Speed * Time.deltaTime, Space.World);
+           // go.transform.Rotate(Vector3.right, JS.Vertical * Speed * Time.deltaTime, Space.World);
+        }
+    }
+}
---
+++ Assets/Scripts/CubeMover.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class CubeMover : MonoBehaviour,IDragHandler
+{
+    public GameObject parent;
+    public void OnDrag(PointerEventData eventData)
+    {
+         if(Mathf.Abs(eventData.delta.x)> Mathf.Abs(eventData.delta.y))
+        {
+         if (Vector3.Angle(transform.forward, Camera.main.transform.forward) > 45)
+            {
+              transform.position=Vector3.MoveTowards(transform.position,transform.position + Mathf.Sign(eventData.delta.x)*2*transform.forward, Time.deltaTime * 10);
+            }
+        }
+
+
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
